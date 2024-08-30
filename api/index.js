a558b537c2eb52be0138ca29cdf1acf314990f82
Moksh Goyal <mokshgoyal616@gmail.com>
2024-08-30 18:30:57 +0530

    Minor Changes

--- api/index.js
+++ api/index.js
@@ -14,6 +14,6 @@ mongoose
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
 });
