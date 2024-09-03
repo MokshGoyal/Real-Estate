de30214abf9069b91f70f7c5925fba80855cd2bd
Moksh Goyal <mokshgoyal616@gmail.com>
2024-09-03 10:57:04 +0530

    Completed CE-1

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,5 @@
 import { useSelector } from "react-redux";
-import { useRef, useState, useEffect } from "react";
-import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
-import { storage } from "../firebase.js"; // Import storage from your firebase.js file
+import { useState } from "react";
 import {
   updateUserSuccess,
   updateUserFailure,
@@ -13,49 +11,9 @@ import { useDispatch } from "react-redux";
 
 export default function Profile() {
   const { currentUser, loading, error } = useSelector((state) => state.user);
-  const [file, setFile] = useState(undefined);
-  const [filePerc, setFilePerc] = useState(0);
-  const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
-  const fileRef = useRef();
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const dispatch = useDispatch();
-
-  // File upload logic
-  useEffect(() => {
-    if (file) {
-      handleFileUpload();
-    }
-  }, [file]);
-
-  const handleFileUpload = () => {
-    const fileName = new Date().getTime() + file.name;
-    const storageRef = ref(storage, fileName);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setFilePerc(Math.round(progress));
-      },
-      (error) => {
-        console.error("Upload error:", error);
-        setFileUploadError(true);
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref)
-          .then((downloadURL) => {
-            setFormData((prev) => ({ ...prev, avatar: downloadURL }));
-          })
-          .catch((error) => {
-            console.error("Error fetching download URL:", error);
-            setFileUploadError(true);
-          });
-      }
-    );
-  };
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -82,12 +40,14 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message || "unknown error"));
     }
   };
+
   const handleDeleteUser = async () => {
     try {
       dispatch(deleteUserStart());
       const req = await fetch(`/api/user/delete/${currentUser._id}`, {
-        method: 'DELETE'
+        method: "DELETE",
       });
+      const data = await req.json();
       if (data.success === false) {
         dispatch(deleteUserFailure(data.message));
         return;
@@ -97,42 +57,18 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input
-          onChange={(e) => setFile(e.target.files[0])}
-          type="file"
-          ref={fileRef}
-          hidden
-          accept="image/*"
-        />
-        <img
-          onClick={() => fileRef.current.click()}
-          src={formData.avatar || currentUser.avatar}
-          alt="profile"
-          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
-        />
-        <p className="text-sm self-centered">
-          {fileUploadError ? (
-            <span className="text-red-700">
-              Error: Image upload (image must be less than 2 MB)
-            </span>
-          ) : filePerc > 0 && filePerc < 100 ? (
-            <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
-          ) : filePerc === 100 ? (
-            <span className="text-green-700">Image successfully uploaded!</span>
-          ) : (
-            ""
-          )}
-        </p>
         <input
           type="text"
           placeholder="Username"
           defaultValue={currentUser.username}
           id="username"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
@@ -156,7 +92,12 @@ export default function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete Account
+        </span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? "request ok" : ""}</p>
